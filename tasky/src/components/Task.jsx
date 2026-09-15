a40c106766a1775268cd9c73b9933c1e76583782
Alwin <20112504@mail.wit.ie>
2026-09-15 12:20:38 +0100

    Completed Tasky Lab 1

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,8 +1,16 @@
-const Task = () => {
+const Task = (props) => {
     
     return (
-        <p>This is a task!</p>
+        <div className="card">
+            <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p>{props.children}</p>
+        </div>
     )
+
+
+
 }
+
 
 export default Task;
