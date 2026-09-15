f57d9fb11164667edc584f43680f83d73e1bbf5c
Alwin <20112504@mail.wit.ie>
2026-09-15 12:34:54 +0100

    Completed Tasky Lab 1

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,15 +3,11 @@ import Task from './components/Task';
 
 function App() {
   return (
-        <div className="container">
-
+    <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-          <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-    </Task>
-
-      <Task title="Tidy" deadline="Today" />
+      <Task title="Dishes" deadline="Today" description="Wash, dry, and put away all dishes in the sink" />
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away" />
+      <Task title="Tidy" deadline="Today" description="Straighten up the living room and clear clutter" />
     </div>
   );
 }
