af4670da5dcde3b7a1f38ff9b21a873cb476cdaa
Alwin <20112504@mail.wit.ie>
2026-09-22 11:54:37 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -39,6 +39,13 @@ function App() {
     setTaskState({ tasks });
   };
 
+    const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -52,6 +59,8 @@ function App() {
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
+
         />
       ))}
     </div>
--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -15,6 +15,8 @@ const Task = (props) => {
             <button onClick={props.markDone} className="doneButton">
                 Done
             </button>
+                <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+
         </div>
     );
 
